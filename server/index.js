d96f8752b33d16a7a01b891357298ca4895a2008
Toheedullah-K3 <toheedullah002@gmail.com>
2025-01-26 17:46:27 +0500

    Added Inital Folders

--- server/index.js
+++ server/index.js
@@ -7,6 +7,11 @@ app.get('/', (req, res) => {
   res.send('I Am Running a Server');
 });
 
+app.get('/dummy', (req, res) => {
+    res.send('Dummy Data Running');
+    const {data} = req.body;
+  });
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
