9be3d27e4d06043c8fbb542233eb4a8f4313c17d
Toheedullah-K3 <toheedullah002@gmail.com>
2025-02-02 23:50:35 +0500

    Initial Routes Setup in CLient

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,50 +1,12 @@
-import { useState } from "react";
-import axios from "axios";
+import React from "react";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  const [formData, setFormData] = useState({ name: "", email: "" });
-
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await axios.post("http://localhost:3000/submit", formData);
-      alert(response.data.message);
-    } catch (error) {
-      console.error("Error submitting form:", error);
-    }
-  };
 
   return (
-    <div className="p-6 bg-gray-100 min-h-screen flex flex-col items-center">
-      <h1 className="text-lg font-bold mb-4">Simple Form</h1>
-      <form onSubmit={handleSubmit} className="bg-white p-4 rounded shadow-md">
-        <input
-          type="text"
-          name="name"
-          placeholder="Enter Name"
-          value={formData.name}
-          onChange={handleChange}
-          className="border p-2 w-full mb-2"
-          required
-        />
-        <input
-          type="email"
-          name="email"
-          placeholder="Enter Email"
-          value={formData.email}
-          onChange={handleChange}
-          className="border p-2 w-full mb-2"
-          required
-        />
-        <button type="submit" className="bg-blue-500 text-white p-2 rounded">
-          Submit
-        </button>
-      </form>
-    </div>
+   <>
+    <Outlet />
+   </>
   );
 }
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
-import './assets/styles/style.css'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+
+// pages
 import App from './App.jsx'
+import Home from './pages/Home.jsx'
+import Login from './pages/Login.jsx'
+import Signup from './pages/Signup.jsx'
+import UserProfile from './pages/UserProfile.jsx'
+
+
+const routes = createRoutesFromElements(
+  <Route element={<App />}>
+    <Route path="/" element={<Home />} />
+    <Route path="/login" element={<Login />} />
+    <Route path="/signup" element={<Signup />} />
+    <Route path="/user-profile" element={<UserProfile />} />
+  </Route>,
+)
+
+const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <RouterProvider router={router} />,
 )
