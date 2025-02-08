a81c69d4e3070905e7a1793288e762aee96f9d55
Toheedullah-K3 <toheedullah002@gmail.com>
2025-02-09 00:48:03 +0500

    Working with User Model

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,27 +1,51 @@
 import mongoose, { Schema } from "mongoose";
-
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const userSchema = new Schema(
     {
         username: {
             type: String,
             required: true,
-            unique: true
+            lowercase: true,
+            unique: true,
+            trim: true,
+            index: true
         },
         email: {
             type: String,
             required: true,
+            lowercase: true,
+            trime: true,
             unique: true
         },
         password: {
             type: String,
             required: true
+        },
+        refreshToken: {
+            type: String
         }
     },
     {
         timestamps: true
     }
 );
+
+userSchema.pre("save", async function(next){
+    if(!this.isModified("password")) return next();
+
+    try {
+        this.password = await bcrypt.hash(this.password, 10);
+        return next();
+    } catch (error) {
+        next(error);
+    }
+})
+
+userSchema.methods.comparePassword = async function(password){
+    return await bcrypt.compare(password, this.password);
+}
 
 
 const User = mongoose.model('User', userSchema);
