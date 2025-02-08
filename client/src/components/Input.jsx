51154c2733f7bc522d24a64490a0244d2e3d5e99
Toheedullah-K3 <toheedullah002@gmail.com>
2025-02-08 17:16:13 +0500

    Added Input Component to Login Page

--- client/src/components/Input.jsx
+++ client/src/components/Input.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 
-const Input = () => {
-  return (
-    <div>Input</div>
-  )
-}
+const Input = React.forwardRef(function Input({
+    type = 'text',
+    placeholder = 'Enter text',
+    className = '',
+    ...props
+}, ref) {
+    return (
+        <input
+            type={type}
+            placeholder={placeholder}
+            className={`w-full bg-transparent outline-none text-white text-lg pl-2 ${className}`}
+            ref={ref}
+            {...props}
+
+        />
+    )
+})
 
 export default Input
