f80d5bac9efaf9851ae0bb4c853f019f4c719810
Toheedullah-K3 <toheedullah002@gmail.com>
2025-02-09 01:05:07 +0500

    Access & Refresh TOken added

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -47,6 +47,21 @@ userSchema.methods.comparePassword = async function(password){
     return await bcrypt.compare(password, this.password);
 }
 
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        { _id: this._id },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+    )
+};
+
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        { _id: this._id },
+        process.env.REFRESH_TOKEN_SECRET,
+        { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
+    )
+};
 
 const User = mongoose.model('User', userSchema);
 export default User;
