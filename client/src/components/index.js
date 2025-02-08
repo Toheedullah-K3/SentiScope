10cf626396cc74ff38d8365ef3a6974ca5b22cdb
Toheedullah-K3 <toheedullah002@gmail.com>
2025-02-08 17:01:40 +0500

    Input Component Added

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,8 +1,11 @@
 import Navbar from "./Header/Navbar.jsx";
 import Button from "./Button.jsx";
 import Hero from "./Hero.jsx";
+import Input from "./Input.jsx";
+
 export { 
     Navbar,
     Button,
-    Hero
+    Hero,
+    Input
 };
