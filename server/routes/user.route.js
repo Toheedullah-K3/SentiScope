48b745af7edd9e5d11d0509ed77cd0689a1d6ee2
Toheedullah-K3 <toheedullah002@gmail.com>
2025-02-10 17:39:01 +0500

    login controller method change

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -5,6 +5,6 @@ import { registerUser, loginUser } from '../controllers/user.controller.js';
 const router = Router();
 
 router.route('/register').post(registerUser);
-router.route('/login').get(loginUser);
+router.route('/login').post(loginUser);
 
 export default router;
