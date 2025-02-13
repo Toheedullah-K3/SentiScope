08863459668cb78d5ae31ecf6fa68157c694aa71
Toheedullah-K3 <toheedullah002@gmail.com>
2025-02-13 15:21:17 +0500

    auth middleware created

---
+++ server/middlewares/auth.middleware.js
@@ -0,0 +1,26 @@
+import jwt from 'jsonwebtoken';
+import { User } from '../models/user.model.js';
+
+export const VerifyJWT = async (req, res, next) => {
+    try {
+        const token = req.cookies?.accessToken || req.header('Authorization')?.replace('Bearer ', '');
+
+        if (!token) {
+            return res.status(401).json({ message: 'Access Token not found' });
+        }
+
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+        const user = await User.findById(decodedToken._id);
+
+        if (!user) {
+            return res.status(401).json({ message: 'User not found' });
+        }
+
+        req.user = user;
+        next();
+    } catch (error) {
+        console.log(error);
+        return res.status(401).json({ message: 'Invalid Access Token' });
+    }
+}
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,11 +1,11 @@
 import { Router } from 'express';
 import { registerUser, loginUser, logoutUser } from '../controllers/user.controller.js';
-
+import { VerifyJWT } from '../middlewares/auth.middleware.js';
 
 const router = Router();
 
 router.route('/register').post(registerUser);
 router.route('/login').post(loginUser);
-router.route('/logout').post(logoutUser)
+router.route('/logout').post(VerifyJWT, logoutUser);
 
 export default router;
