b8e2779632ef1ea840248128dde703b41a4867fc
Toheedullah-K3 <toheedullah002@gmail.com>
2025-02-28 21:08:10 +0500

    modify searchRequest Model

--- server/models/searchRequest.model.js
+++ server/models/searchRequest.model.js
@@ -7,7 +7,24 @@ const searchRequestSchema = new Schema(
             lowercase: true,
             required: true,
             trim: true
+        },
+        platform:{
+            type: [String],
+            enum: ["Reddit", "Twitter", "Facebook"],
+            default: ["Twitter"]
+        },
+        model: {
+            type: [String],
+            enum: ["Textblob", "Vader", "GenAI"],
+            default: ["Textblob"]
+        },
+        userId: {
+            type: Schema.Types.ObjectId,
+            ref: "User"
         }
+    },
+    {
+        timestamps: true
     }
 )
 
